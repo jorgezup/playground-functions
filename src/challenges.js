a4c5d06ff206ee794c6abe60ce7b6d8468190565
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-11-11 13:35:40 -0800

    Concluido nono requisito obrigatorio

--- src/challenges.js
+++ src/challenges.js
@@ -40,9 +40,9 @@ function highestCount(arrayNumbers) {
   //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
   const highestNumber = Math.max(...arrayNumbers)
   let count = 0
-  for (const n of arrayNumbers) {
+  for (let n of arrayNumbers) {
     if (n === highestNumber) {
-      count++
+      count += 1
     }
   }
   return count
@@ -53,20 +53,20 @@ function catAndMouse(mouse, cat1, cat2) {
   const distCat1 = Math.abs(mouse - cat1)
   const distCat2 = Math.abs(mouse - cat2)
   if (distCat1 === distCat2) {
-    return "os gatos trombam e o rato foge"
+    return 'os gatos trombam e o rato foge'
   }
   else if (distCat1 > distCat2) {
-    return "cat2"
+    return 'cat2'
   }
   else {
-    return "cat1"
+    return 'cat1'
   }
 }
 
 // Desafio 8
 function fizzBuzz(arrayNumbers) {
   const newArray = []
-  for (n of arrayNumbers) {
+  for (let n of arrayNumbers) {
     if (n % 3 === 0 && n % 5 !== 0) {
       newArray.push('fizz')
     }
@@ -84,11 +84,45 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let encodedString = ''
+  const encodeList = {
+    'a': 1,
+    'e': 2,
+    'i': 3,
+    'o': 4,
+    'u': 5,
+  }
+
+  for (let char of str) {
+    if (char in encodeList) {
+      encodedString += encodeList[char]
+    }
+    else {
+      encodedString += char
+    }
+  }
+  return encodedString
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  let decodedString = ''
+  const decodeList = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  }
+
+  for (let char of str) {
+    if (char in decodeList) {
+      decodedString += decodeList[char]
+    }
+    else {
+      decodedString += char
+    }
+  }
+  return decodedString
 }
 
 module.exports = {
